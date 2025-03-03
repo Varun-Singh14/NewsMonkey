a89fb0aafd3e212d97522a2161522411e203d5e0
Varun-Singh14 <varunom123singh@gmail.com>
2025-03-03 14:47:43 +0530

    Fetching News from API and adding Pagination

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,60 +3,70 @@ import NewsItem from "./NewsItem";
 
 export default class News extends Component {
 
-  articles = [
-    {
-      "source": { "id": "bbc-sport", "name": "BBC Sport" },
-      "author": null,
-      "title": "England cricket: 'Brendon McCullum's problems go far beyond Jos Buttler's future'",
-      "description": "England head coach Brendon McCullum faces bigger problems than Jos Buttler's future following a frustrating Champions Trophy campaign.",
-      "url": "http://www.bbc.co.uk/sport/cricket/articles/cp9yy25dg5yo",
-      "urlToImage": "https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/949e/live/1dbb36d0-f4f8-11ef-896e-d7e7fb1719a4.jpg",
-      "publishedAt": "2025-02-27T11:37:21.2813524Z",
-      "content": "England's focus now switches to the challenge McCullum faces.\r\nWhen taking on the white-ball role, he said his coaches would step up to lead when he needed time away but evidence suggests this team c… [+2758 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
-
   constructor() {
     super();
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+  
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults
+    });
+  }
+  
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1
+    })
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)) {
+      // TODO
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1
+      })
     }
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem
-              title="myTitle"
-              description="myDesc"
-              imgUrl="https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/949e/live/1dbb36d0-f4f8-11ef-896e-d7e7fb1719a4.jpg"
-              newsUrl="TODO"
-            />
-          </div>
+          {this.state.articles.map((item) => {
+            return (
+              <div key={item.url} className="col-md-4">
+                <NewsItem
+                  title={item.title ? item.title : ""}
+                  description={item.description ? item.description : ""}
+                  imgUrl={item.urlToImage ? item.urlToImage : "https://imgs.search.brave.com/INcL1UlEApJSSGbLNaE3EaK6CePCvs9hWWMg1kzyIdA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9vbmxpbmUtbmV3/cy1jb25jZXB0XzE3/MjQyOS04NDAuanBn/P3NlbXQ9YWlzX2h5/YnJpZA"}
+                  newsUrl={item.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
+          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </div>
     );
