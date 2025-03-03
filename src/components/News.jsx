c2585deb0d773edcd0f74971dc9a177050560b57
Varun-Singh14 <varunom123singh@gmail.com>
2025-03-03 21:14:40 +0530

    Refactoring Code

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -25,10 +25,10 @@ export default class News extends Component {
       page: 1
     }
   }
-  
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
@@ -37,37 +37,31 @@ export default class News extends Component {
       loading: false
     });
   }
-  
-  handlePreviousClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
-      page: this.state.page - 1,
+      totalResults: parsedData.totalResults,
       loading: false
-    })
+    });
+  }
+
+  handlePreviousClick = async () => {
+    this.setState({ page: this.state.page - 1 }, () => { this.updateNews(); } );
   }
 
   handleNextClick = async () => {
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=5e0aad4aee254cc6a13a98097acca03c&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true});
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        articles: parsedData.articles,
-        page: this.state.page + 1,
-        loading: false
-      })
-    }
+    this.setState({ page: this.state.page + 1 }, () => { this.updateNews(); } );
   }
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin: '35px 0px'}}>NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading && this.state.articles.map((item) => {
@@ -78,6 +72,9 @@ export default class News extends Component {
                   description={item.description ? item.description : ""}
                   imgUrl={item.urlToImage ? item.urlToImage : "https://imgs.search.brave.com/INcL1UlEApJSSGbLNaE3EaK6CePCvs9hWWMg1kzyIdA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9vbmxpbmUtbmV3/cy1jb25jZXB0XzE3/MjQyOS04NDAuanBn/P3NlbXQ9YWlzX2h5/YnJpZA"}
                   newsUrl={item.url}
+                  author={item.author}
+                  date={item.publishedAt}
+                  source={item.source.name}
                 />
               </div>
             );
@@ -85,7 +82,7 @@ export default class News extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </div>
     );
